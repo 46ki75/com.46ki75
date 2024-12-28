6cd4d74522bd1faf6e4c18658925a579ec60d377
shirayuki <shirayuki@46ki75.com>
2024-12-29 08:43:09 +0900

    feat(graphql): implement SessionStorageCache class for session storage management

--- packages/graphql/src/execute.ts
+++ packages/graphql/src/execute.ts
@@ -12,6 +12,54 @@ abstract class Cache<T> {
   abstract get(): T | undefined
 }
 
+class SessionStorageCache<T> extends Cache<T> {
+  /**
+   * Remove the item from the cache
+   */
+  remove(): void {
+    if (typeof window !== 'undefined') {
+      sessionStorage.removeItem(this.key)
+    }
+  }
+
+  /**
+   *  Set the item in the cache
+   * @param param0 The max age of the cache and the data to store
+   */
+  set({ maxAge, data }: { maxAge: number; data: T }): void {
+    if (typeof window !== 'undefined') {
+      const expires = new Date(Date.now() + maxAge)
+      sessionStorage.setItem(this.key, JSON.stringify({ expires, data }))
+    }
+  }
+
+  /**
+   * Get the item from the cache
+   * @returns The data from the cache or `undefined` if the cache is expired
+   */
+  get(): T | undefined {
+    if (typeof window === 'undefined') {
+      return undefined
+    } else {
+      const item = sessionStorage.getItem(this.key)
+
+      if (item == null) return undefined
+
+      const cache: {
+        expires: string
+        data: T
+      } = JSON.parse(item)
+
+      if (new Date(cache.expires) < new Date()) {
+        this.remove()
+        return undefined
+      }
+
+      return cache.data
+    }
+  }
+}
+
 class LocalStorageCache<T> extends Cache<T> {
   /**
    * Remove the item from the cache

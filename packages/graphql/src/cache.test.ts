0d4be4f14708d00947ae9cf62d83f17caae2ce46
shirayuki <shirayuki@46ki75.com>
2024-12-29 09:02:03 +0900

    test(graphql): add tests for SessionStorageCache functionality and update LocalStorageCache test cases

--- packages/graphql/src/cache.test.ts
+++ packages/graphql/src/cache.test.ts
@@ -4,7 +4,7 @@
  */
 
 import { expect, test } from 'vitest'
-import { LocalStorageCache } from './cache'
+import { LocalStorageCache, SessionStorageCache } from './cache'
 import { describe } from 'node:test'
 
 describe('LocalStorageCache', async () => {
@@ -12,7 +12,7 @@ describe('LocalStorageCache', async () => {
     type Data = { keyA: number; keyB: string; keyC: boolean }
     const data = { keyA: 1, keyB: 'test', keyC: true }
 
-    const cache = new LocalStorageCache<Data>('Set and Get')
+    const cache = new LocalStorageCache<Data>('LocalStorageCache:Set and Get')
     cache.set({ maxAge: 1000, data })
 
     expect(cache.get()).toEqual(data)
@@ -22,7 +22,29 @@ describe('LocalStorageCache', async () => {
     type Data = { keyA: number; keyB: string; keyC: boolean }
     const data = { keyA: 1, keyB: 'test', keyC: true }
 
-    const cache = new LocalStorageCache<Data>('Get expired')
+    const cache = new LocalStorageCache<Data>('LocalStorageCache:Get expired')
+    cache.set({ maxAge: -1, data })
+
+    expect(cache.get()).toEqual(undefined)
+  })
+})
+
+describe('SessionStorageCache', async () => {
+  test('Set and Get', async () => {
+    type Data = { keyA: number; keyB: string; keyC: boolean }
+    const data = { keyA: 1, keyB: 'test', keyC: true }
+
+    const cache = new SessionStorageCache<Data>('LocalStorageCache:Set and Get')
+    cache.set({ maxAge: 1000, data })
+
+    expect(cache.get()).toEqual(data)
+  })
+
+  test('Get expired', async () => {
+    type Data = { keyA: number; keyB: string; keyC: boolean }
+    const data = { keyA: 1, keyB: 'test', keyC: true }
+
+    const cache = new SessionStorageCache<Data>('LocalStorageCache:Get expired')
     cache.set({ maxAge: -1, data })
 
     expect(cache.get()).toEqual(undefined)
